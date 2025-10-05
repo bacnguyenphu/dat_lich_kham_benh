44cfa17cbac1825be30ec020a6436f82dc44c0fd
bacnguyen <bac03906@gmail.com>
2025-10-05 23:20:16 +0700

    chuan bij lam debounce

--- backend/src/services/searchService.js
+++ backend/src/services/searchService.js
@@ -20,15 +20,19 @@ const search = async (value, filter) => {
                 whereDoctor = { [Op.or]: [{ firstName: value }, { lastName: value }] }
                 whereSpecialtyAndPackage.name = { [Op.like]: `%${value}%` };
             }
+            else {
+                whereDoctor = {}
+                whereSpecialtyAndPackage = {};
+            }
 
             dataDoctor.name = "doctor"
             dataDoctor.data = await db.Doctor.findAll({
-                attributes: ['id', 'price', 'updatedAt',],
+                attributes: ['id'],
                 include: [
                     {
                         model: db.User,
                         as: 'user',
-                        attributes: ['firstName', 'lastName', 'phone', 'email', 'dateOfBirth', 'gender', 'address', 'avatar'],
+                        attributes: ['firstName', 'lastName', 'avatar'],
                         where: whereDoctor,
                     },
                     { model: db.Position, as: 'position', attributes: ['name'], through: { attributes: [] } },
@@ -37,10 +41,71 @@ const search = async (value, filter) => {
                 limit: 5,
             })
 
-            dataSpecialty.name = "specialties"
+            dataSpecialty.name = "specialty"
             dataSpecialty.data = await db.Specialty.findAll({
                 where: whereSpecialtyAndPackage,
+                attributes: ['id', 'name', 'images',],
                 limit: 5
+            })
+
+            dataPackage.name = "package"
+            dataPackage.data = await db.Medical_package.findAll({
+                where: whereSpecialtyAndPackage,
+                attributes: ['id', 'name', 'image'],
+                include: [
+                    { model: db.Category_package, as: 'category_package', attributes: ['id', 'name'] },
+                ]
+            })
+
+            if (dataDoctor.data.length > 0) {
+                data.push(dataDoctor)
+            }
+            if (dataSpecialty.data.length > 0) {
+                data.push(dataSpecialty)
+            }
+            if (dataPackage.data.length > 0) {
+                data.push(dataPackage)
+            }
+
+        }
+        else if (filter === 'doctor') {
+            if (value) {
+                whereDoctor = { [Op.or]: [{ firstName: value }, { lastName: value }] }
+            }
+            else {
+                whereDoctor = {}
+            }
+            dataDoctor.name = "doctor"
+            dataDoctor.data = await db.Doctor.findAll({
+                attributes: ['id'],
+                include: [
+                    {
+                        model: db.User,
+                        as: 'user',
+                        attributes: ['firstName', 'lastName', 'avatar'],
+                        where: whereDoctor
+                    },
+                    { model: db.Position, as: 'position', attributes: ['name'], through: { attributes: [] } },
+                    { model: db.Specialty, as: 'specialty', attributes: ['name'], through: { attributes: [] } }
+                ],
+                limit: 15,
+            })
+            if (dataDoctor.data.length > 0) {
+                data.push(dataDoctor)
+            }
+        }
+        else if (filter === 'specialty') {
+            if (value) {
+                whereSpecialtyAndPackage.name = { [Op.like]: `%${value}%` };
+            }
+            else {
+                whereSpecialtyAndPackage= {};
+            }
+            dataSpecialty.name = "specialty"
+            dataSpecialty.data = await db.Specialty.findAll({
+                where: whereSpecialtyAndPackage,
+                attributes: ['id', 'name', 'images',],
+                limit: 15
             })
 
             dataPackage.name = "package"
@@ -51,40 +116,29 @@ const search = async (value, filter) => {
                     { model: db.Category_package, as: 'category_package', attributes: ['id', 'name', 'image', 'description'] },
                 ]
             })
-
-            if (dataDoctor.data) {
-                data.push(dataDoctor)
-            }
-            if (dataSpecialty.data) {
+            if (dataSpecialty.data.length > 0) {
                 data.push(dataSpecialty)
             }
-            if (dataPackage.data) {
+        }
+        else if (filter === 'package') {
+            if (value) {
+                whereSpecialtyAndPackage.name = { [Op.like]: `%${value}%` };
+            }
+            else {
+                whereSpecialtyAndPackage.name = {};
+            }
+            dataPackage.name = "package"
+            dataPackage.data = await db.Medical_package.findAll({
+                where: whereSpecialtyAndPackage,
+                attributes: ['id', 'name', 'image'],
+                include: [
+                    { model: db.Category_package, as: 'category_package', attributes: ['id', 'name'] },
+                ],
+                limit:15
+            })
+            if (dataPackage.data.length > 0) {
                 data.push(dataPackage)
             }
-
-        }
-        else if (filter === 'doctor') {
-            if (value) {
-                whereDoctor = {
-                    [Op.or]: [{ firstName: { [Op.like]: `%${value}%` } }, { lastName: { [Op.like]: `%${value}%` } }]
-                }
-            }
-            dataDoctor.name = "doctor"
-            dataDoctor.data = await db.Doctor.findAll({
-                attributes: ['id', 'price', 'updatedAt',],
-                include: [
-                    {
-                        model: db.User,
-                        as: 'user',
-                        attributes: ['firstName', 'lastName', 'phone', 'email', 'dateOfBirth', 'gender', 'address', 'avatar'],
-                        where: whereDoctor
-                    },
-                    { model: db.Position, as: 'position', attributes: ['name'], through: { attributes: [] } },
-                    { model: db.Specialty, as: 'specialty', attributes: ['name'], through: { attributes: [] } }
-                ],
-                limit: 15,
-            })
-            data.push(dataDoctor)
         }
 
         return {
--- frontend/src/pages/Search.jsx
+++ frontend/src/pages/Search.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { FiSearch } from "react-icons/fi";
 import { search } from "../services/search";
+import { useEffect } from "react";
+import default_image from '../assets/default_image.webp'
+import defaultAvatar from '../assets/defaultAvatar.png'
 
 function InputSearch() {
 
@@ -12,11 +15,47 @@ function InputSearch() {
     const [value, setValue] = useState("")
     const [filter, setFilter] = useState(ALL)
 
-    const handleSearch = async () => {
-        const res = await search(value,filter)
-        console.log("check res>>>",res);
-                
-    }
+    const [doctors, setDoctors] = useState([])
+    const [specialties, setSpecialties] = useState([])
+    const [packages, setPackages] = useState([])
+
+    useEffect(() => {
+        const fetchDataSearch = async () => {
+            const res = await search(value, filter)
+            console.log(res);
+            if (res.err === 0 && res?.data && res.data.length > 0) {
+                let dataDoctors = res.data.find(item => item?.name === DOCTOR)
+                if (dataDoctors) {
+                    setDoctors(dataDoctors.data)
+                }
+                else {
+                    setDoctors([])
+                }
+
+                let dataSpecialties = res.data.find(item => item?.name === SPECIALTY)
+                if (dataSpecialties) {
+                    setSpecialties(dataSpecialties.data)
+                }
+                else {
+                    setSpecialties([])
+                }
+
+                let dataPackages = res.data.find(item => item.name === PACKAGE)
+                if (dataPackages) {
+                    setPackages(dataPackages.data)
+                }
+                else {
+                    setPackages([])
+                }
+            }
+        }
+
+        fetchDataSearch()
+    }, [filter])
+
+    // console.log('check doctor', doctors);
+    // console.log('check specialty', specialties);
+    // console.log('check package', packages);
 
     return (
         <div className="lg:px-40 md:px-20 px-5">
@@ -24,7 +63,7 @@ function InputSearch() {
                 <span className="">
                     <FiSearch />
                 </span>
-                <input className="grow p-1 mx-3 outline-none" value={value} onChange={(e)=>{setValue(e.target.value)}} />
+                <input className="grow p-1 mx-3 outline-none" value={value} onChange={(e) => { setValue(e.target.value) }} />
                 <select className="outline-none bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5"
                     defaultValue={ALL}
                     onChange={(e) => { setFilter(e.target.value) }}
@@ -35,7 +74,65 @@ function InputSearch() {
                     <option value={PACKAGE}>Gói khám</option>
                 </select>
             </div>
-            <button onClick={() => { handleSearch() }}>search</button>
+            <div className="mt-10">
+                {specialties.length > 0 &&
+                    <div>
+                        <p className="font-semibold bg-[#F3F4F6] py-1 px-2">Chuyên khoa</p>
+                        {specialties.map((item, i) => {
+                            const length = specialties.length
+                            return (
+                                <div key={item.id}>
+                                    <div className="flex items-center gap-4 py-3">
+                                        <img className="size-9 object-center object-cover" src={(item?.images) ? item?.images : default_image} />
+                                        <p>{item?.name}</p>
+                                    </div>
+                                    {i !== length - 1 && <hr className="border- border-gray-300" />}
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
+                {doctors.length > 0 &&
+                    <div>
+                        <p className="font-semibold bg-[#F3F4F6] py-1 px-2">Bác sĩ</p>
+                        {doctors.map((item, i) => {
+                            const length = doctors.length
+                            return (
+                                <div key={item.id}>
+                                    <div className="flex items-center gap-4 py-3">
+                                        <img className="size-9 object-center object-cover" src={(item?.user?.avatar) ? item?.user?.avatar : defaultAvatar} />
+                                        <div className="">
+                                            <p>{item?.position.map(pos => pos.name).join(" ")} {item?.user?.firstName} {item?.user?.lastName}</p>
+                                            <p className="text-sm text-gray-500">{item?.specialty.map(spe => spe.name).join(" - ")}</p>
+                                        </div>
+                                    </div>
+                                    {i !== length - 1 && <hr className="border- border-gray-300" />}
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
+                {packages.length > 0 &&
+                    <div>
+                        <p className="font-semibold bg-[#F3F4F6] py-1 px-2">Gói khám</p>
+                        {packages.map((item, i) => {
+                            const length = packages.length
+                            return (
+                                <div key={item.id}>
+                                    <div className="flex items-center gap-4 py-3">
+                                        <img className="size-9 object-center object-cover" src={(item?.image) ? item?.image : default_image} />
+                                        <div>
+                                            <p>{item?.name}</p>
+                                            <p className="text-sm text-gray-500">{item?.category_package?.name}</p>
+                                        </div>
+                                    </div>
+                                    {i !== length - 1 && <hr className="border- border-gray-300" />}
+                                </div>
+                            )
+                        })}
+                    </div>
+                }
+            </div>
         </div>
     );
 }
